efd289f20321d6816d22a94ac79a6f3c441bf07c
Barry Niemuth <bn3737@yahoo.com>
2014-03-27 16:30:15 -0500

    Initial push

---
+++ app/views/posts/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('header')
+	<h1 class="blog-title">Edit Post</h1>
+@stop
+
+@section('content')
+<div class="blog-post">
+	{{ Form::model($post, array('action' => array('PostsController@update',$post->id), 'method' => 'put', class'=>'form-horizontal')) }}
+	<div class="form-group {{ $errors->has('title) ? 'has-error' : '' }}">
+	    {{ Form::label('title', 'Title', $attributes = array('class' => 'col-sm-2 control-label')) }}
+	   	<div class="col-sm-10">
+	    {{ Form::text('title', null, $attributes = array('class' => 'form-control', 'placeholder'=>'Title')) }}
+	      {{ $errors->first('title', '<p><span class="help-block">:message</span></p>') }}	    </div>
+	  </div>
+
+	  <div class="form-group">
+	    <label for="body" class="col-sm-2 control-label">Body</label>
+	    <div class="col-sm-10">
+	      <input type="text" class="form-control" id="body" name="body" placeholder="Body" value="{{{ Input::old('body') }}}">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <div class="col-sm-offset-2 col-sm-10">
+	      <div class="checkbox">
+	        <!-- <label>
+	          <input type="checkbox"> Remember me
+	        </label> -->
+	      </div>
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <div class="col-sm-offset-2 col-sm-10">
+	      <button type="submit" class="btn btn-default">Create Post</button>
+	    </div>
+	  </div>
+	{{ Form::close() }}
+	<!-- <form class="form-horizontal" role="form" action="{{{ action('PostsController@store') }}}" method="post">
+	  
+	</form> -->
+
+@stop
