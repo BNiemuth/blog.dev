30cffb7ad6a82fcfa04d2f844586ca2a0ac4bb2e
Barry Niemuth <bn3737@yahoo.com>
2014-03-27 23:55:15 -0500

    Polish | 404 error page

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -2,9 +2,9 @@
 
 @section('header')
 
-	<h1>Whoops now you're lost-I think you shoulda made a left turn at Albuquerque.</h1>
+	<h1>Now you're lost-I think you shoulda made a left turn at Albuquerque.</h1>
 	<h3>(Hit the back arrow and try it again...OK Doc?)</h3>
-	
+
 <div class="col-lg-6 col-lg-offset-3">
 <p><img class="img-responsive" src="css/Bugs_Bunny_Albuquerque.gif" alt=""></p>
 </div>
